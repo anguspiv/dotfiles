df22d8306abdb131296a6896453c039adfea7447
Angus Perkerson <angusp@angusp.com>
2025-09-04 16:52:43 -0700

    Add .config/zsh/aliases.zsh Add .config/zsh/exports.zsh Add .config/zsh/functions.zsh Add .config/zsh/plugins.zsh Remove .zshrc Add .zshrc

---
+++ dot_config/zsh/plugins.zsh
@@ -0,0 +1,16 @@
+# ZPlug Plugin Configuration
+
+# ZPLUG Setup
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+## ZPlug Plugins with optimized loading
+
+# Performance-focused plugins with defer levels
+zplug "hlissner/zsh-autopair", defer:2
+zplug "cpitt/zsh-dotenv", as:plugin, defer:1
+zplug "z-shell/zsh-diff-so-fancy", as:command, use:"bin/", defer:3
+zplug "zdharma-continuum/fast-syntax-highlighting", defer:2
+
+# Load plugins
+zplug load
